6fc05657e61b0829ce5c40571e135e9972a35490
akbowlin <akbowlin@gmail.com>
2017-09-17 20:00:23 -0500

    added redux dev tools

--- quick-edit/src/index.js
+++ quick-edit/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import reducers from './redux';
 import {BrowserRouter} from 'react-router-dom';
-
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunk)));
 
 ReactDOM.render((
     <Provider store={store}>
